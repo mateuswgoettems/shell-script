228136ec3890cdf64436dfaa01368d48b1669b7c
Mateus <welter.mateus@gmail.com>
2021-11-27 11:19:27 +0000

    add script docker

---
+++ install-docker.sh
@@ -0,0 +1,15 @@
+echo "Script Instalacao Docker Ubuntu"
+echo "By Mateus"
+echo "------------------------------------------------------------------------------------"
+
+sudo apt-get update
+
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+echo "Instalando Docker..."
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
